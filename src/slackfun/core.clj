64fad874848dd436f9f48ec4b44c0b3637823e38
reverendken <kenneth.ayers@ieee.org>
2015-05-09 19:09:30 +0700

    More fixes for bots

--- src/slackfun/core.clj
+++ src/slackfun/core.clj
@@ -27,12 +27,15 @@
 (let [callback-fns (atom [])]
   (defn startRealtime [startResponse]
     (let [rt-callback #(doseq [f @callback-fns] (f %))]
-      (ws/connect (:url (:body startResponse)) :on-receive #(rt-callback (json/read-str %)))))
+      (ws/connect (:url (:body startResponse)) :on-receive #(rt-callback (json/read-str % :key-fn keyword)))))
   (defn add-realtime-callback [f]
     (swap! callback-fns conj f)))
 
 (defn get-slack-token []
   (clojure.string/trim (slurp (str (System/getProperty "user.home") "/.slack/token"))))
+
+(defn get-user-name [user-id]
+  (:name (first (filter #(= user-id (:id %)) (:users (:body @slack-info))))))
 
 (defn slack-connect []
   (startRealtime (reset! slack-info (rtmStart (get-slack-token)))))
@@ -40,17 +43,21 @@
 (defn get-slack-conn []
   (or @slack-conn (reset! slack-conn (slack-connect))))
 
+(defn is-real-user? [user-name]
+  (do
+    (get-slack-conn)
+    (not (nil? (first
+                 (filter #(= user-name (:name %)) (:users (:body @slack-info))))))))
+
 (defn sendMessage [conf message]
   (let [message-id (next-message-id)]
     (ws/send-msg (get-slack-conn) (json/write-str {:id message-id :type "message" :channel (get-conference-id conf) :text message}))))
 
-(defn send-message-post [conf message user-id emoji]
-  (let [channel (get-conference-id conf)
-        message-id (next-message-id)]
-    (client/post (str (url SLACK_URL POST_MESSAGE))
-                 {:query-params {"token" (get-slack-token)
-                                 "channel" channel
-                                 "text" message
-                                 "username" user-id
-                                 "as_user" false
-                                 "icon_emoji" emoji}})))
+(defn send-message-post [channel message user-id emoji]
+  (client/post (str (url SLACK_URL POST_MESSAGE))
+               {:query-params {"token" (get-slack-token)
+                               "channel" channel
+                               "text" message
+                               "username" user-id
+                               "as_user" false
+                               "icon_emoji" emoji}}))
